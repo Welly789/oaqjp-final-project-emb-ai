a6008b1fc6f6856454f179627cdc0e632da20142
Welly789 <oliwelbourn1@gmail.com>
2024-04-06 02:29:16 -0400

    Finished project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -10,11 +10,23 @@ def emotion_detector(text_to_analyze):
 
     # format response into a dictionary
     formatted_response = json.loads(response.text)
-    # extract just the emotions from the dictionary
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    # find the emotion with the maximum value in the dictionary
-    dominant_emotion = max(emotions, key=emotions.get)
-    # add a key/value pair to the emotions dictionary to show the dominant emotion
-    emotions['dominant_emotion'] = dominant_emotion
+
+    # incorporate error handling
+    if response.status_code == 200:
+        # extract just the emotions from the dictionary
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        # find the emotion with the maximum value in the dictionary
+        dominant_emotion = max(emotions, key=emotions.get)
+        # add a key/value pair to the emotions dictionary to show the dominant emotion
+        emotions['dominant_emotion'] = dominant_emotion
+    elif response.status_code == 400:
+        emotions = {
+        "anger": None, 
+        "disgust": None, 
+        "fear": None, 
+        "joy": None, 
+        "sadness": None, 
+        "dominant_emotion": None
+        }
     # return the emotions dictionary in the requested format
     return emotions 
--- server.py
+++ server.py
@@ -1,17 +1,32 @@
-from EmotionDetection.emotion_detection import emotion_detector
+"""
+Code to implement a flask server using IBM emotion 
+detector to detect the emotion of a text input
+"""
 from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route('/emotionDetector')
-def emot_detect(text_to_analyse):
+def emot_detect():
+    """
+    Function to call emotion detection method and send that to the /emotionDetector route
+    """
     text_to_analyse = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyse)
-    return "For the given statement the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}".format(
-        response['anger'],
-        response['disgust'],
-        response['fear'],
-        response['joy'],
-        response['sadness'],
-        response['dominant_emotion']
-    )
+    if response['dominant_emotion'] is not None:
+        output = f"For the given statement the system response is 'anger': {response['anger']}, \
+        'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and \
+        'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}"
+        return output
+    return "Invalid text! Please try again!"
+
+@app.route('/')
+def index():
+    """
+    Function to render the index page
+    """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
